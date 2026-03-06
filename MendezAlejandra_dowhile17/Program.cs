e20a66b6d977f5c1d261d0b440e8efdc8226b093
Alejandra <Alejandra@DESKTOP-HQKA5R3>
2026-03-05 20:37:25 -0600

    uso de do while

--- MendezAlejandra_dowhile17/Program.cs
+++ MendezAlejandra_dowhile17/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Cree un programa que permita ingresar números y cuente cuántos de ellos son pares hasta que el usuario ingrese 0");
+
+int numero;
+int contador = 0;
+
+do
+{
+    Console.WriteLine("Ingrese un número: ");
+    numero = int.Parse(Console.ReadLine());
+
+    if (numero != 0 && numero % 2 == 0)
+    {
+        contador++;
+    }
+
+} while (numero != 0);
+
+Console.WriteLine($"Cantidad de números pares: {contador}");
